b865ee4d339d952b5f6b1e497ce6b8e14f845087
bopellopa <bellak20@byu.edu>
2023-04-27 11:54:37 -0600

    bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import Picture from './HomePage';
 import styles from './HomePage.module.css';
@@ -7,7 +6,6 @@ import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import MovieList from './Movies';
 import Podcast from './Pod';
 
-// import MovieList from './Movies';
 class HomeBody extends React.Component {
   render() {
     return (
@@ -17,6 +15,19 @@ class HomeBody extends React.Component {
     );
   }
 }
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Picture title="Joel Hilton Movie List Website" />
+        <br></br>
+        <HomeBody />
+      </div>
+    );
+  }
+}
+
 class Conclusion extends React.Component {
   render(): React.ReactNode {
     return <p className={styles.p}>Hope you enjoy your movie!!</p>;
@@ -26,19 +37,17 @@ class Conclusion extends React.Component {
 function App() {
   return (
     <div>
-      <Picture title="Joel Hilton Movie List Website" />
-      <br></br>
-      <HomeBody />
-      <br></br>
       <Router>
-        <nav>
-          <Link className="btn btn-primary" to="/movies">
+        <nav className={styles.nav}>
+          <Link className={styles.nava} to="/movies">
             Movies
           </Link>
-          <Link className="btn btn-primary" to="/Pod">
+
+          <Link className={styles.nava} to="/Pod">
             Podcast
           </Link>
-          <Link className="btn btn-primary" to="/">
+
+          <Link className={styles.nava} to="/">
             Home
           </Link>
         </nav>
@@ -46,7 +55,7 @@ function App() {
         <Routes>
           <Route path="/movies" element={<MovieList />} />
           <Route path="/Pod" element={<Podcast />} />
-          <Route path="/" element={<App />} />
+          <Route path="/" element={<HomePage />} />
         </Routes>
       </Router>
       <br></br>
